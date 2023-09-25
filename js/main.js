a1dd7055f6c4fb4653414e39405413f870a15965
Poorva <122814199+Poorva001@users.noreply.github.com>
2023-09-25 01:27:00 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 // WEB303 Assignment 2
+// WEB303 Assignment 2
+$(document).ready(function () {
+  const content = $("#content");
 
+  function LoadContent(page) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("GET", page, true);
 
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        content.fadeOut(500, function () {
+          content.html(xhttp.responseText);
+          content.fadeIn(500);
+        });
+      }
+    };
+    xhttp.send();
+  }
+
+  $("#prospect").on("click", function () {
+    content.empty();
+    LoadContent("prospect.html");
+  });
+
+  $("#convert").on("click", function () {
+    content.empty();
+    LoadContent("convert.html");
+  });
+
+  $("#retain").on("click", function () {
+    content.empty();
+    LoadContent("retain.html");
+  });
+});
